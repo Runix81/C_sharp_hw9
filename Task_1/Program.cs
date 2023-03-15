577a011039c3b89b3dfc7ca79c4c2cd3215caeab
Sergey <runix1981@gmail.com>
2023-03-15 15:45:44 +0300

    Task print all even numbers from M to N

---
+++ Task_1/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, 
+// которая выведет все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+// Метод ввода значений M и N
+
+(int, int) InputNum(string msg, string message)
+{
+    System.Console.Write($"{msg} -> ");
+    int numM = Convert.ToInt32(Console.ReadLine());
+    //System.Console.WriteLine();
+    System.Console.Write($"{message} -> ");
+    int numN = Convert.ToInt32(Console.ReadLine());
+    return (numM, numN);
+}
+
+
+// Метод вывода результата в консоль
+
+
+void PrintResult(int M, int N)
+{
+    if (M <= N)
+    {
+        if (M % 2 == 0)
+        {
+            System.Console.Write($"{M}, ");
+            M += 2;
+            PrintResult(M,N);
+        }
+        else
+        {
+            M++;
+            PrintResult(M,N);
+        }
+
+    }
+    else 
+    {
+        System.Console.WriteLine();
+        //System.Console.Write("_____________________");
+        
+    }
+    return;
+}
+
+(int M, int N) = InputNum("Введите число M", "Введите число N");
+PrintResult(M,N);
+
+
