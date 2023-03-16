d254f28730daeca28a801e77b613565d80254910
Sergey <runix1981@gmail.com>
2023-03-16 12:54:57 +0300

    Add task about sum elements from M to N

---
+++ Task_2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт
+//  сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+// Метод ввода значений M и N
+
+(int, int) InputNum(string msg, string message)
+{
+    System.Console.Write($"{msg} -> ");
+    int numM = Convert.ToInt32(Console.ReadLine());
+    //System.Console.WriteLine();
+    System.Console.Write($"{message} -> ");
+    int numN = Convert.ToInt32(Console.ReadLine());
+    return (numM, numN);
+}
+
+// Метод подсчета суммы элементов между M и N
+
+int ElementSum(int numM, int numN)
+{
+    if (numM == numN) return numM;
+
+    return numM + ElementSum(numM + 1, numN);
+}
+
+(int M, int N) = InputNum("Введите число M", "Введите число N");
+int res = ElementSum(M, N);
+System.Console.WriteLine($"{res}");
+
